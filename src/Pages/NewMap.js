5111316fdfdb98866a5be8bd9f1e0988a342eede
Azim Uddin Ahamed <azim@sqbear.com.au>
2022-06-24 01:14:55 +0600

    coordinates error

--- src/Pages/NewMap.js
+++ src/Pages/NewMap.js
@@ -1,13 +1,11 @@
 import React from "react";
-import ReactMapboxGl, { GeoJSONLayer, Layer, Feature } from "react-mapbox-gl";
+import ReactMapboxGl, { GeoJSONLayer, Layer, Feature, Popup } from "react-mapbox-gl";
 import * as turf from "@turf/turf";
 import geojsonStyles from "./geojsonStyles";
 import 'mapbox-gl/dist/mapbox-gl.css';
 import { Box, Grid } from '@mui/material';
 // import Button from '@mui/material/Button';
-import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
-import { styled, alpha } from '@mui/material/styles';
 import Divider from '@mui/material/Divider';
 import Paper from '@mui/material/Paper';
 import MenuList from '@mui/material/MenuList';
@@ -56,48 +54,6 @@ const NewMap = () => {
             );
         }
     }, [initCountry]);
-
-    const StyledMenu = styled((props) => (
-        <Menu
-            elevation={0}
-            anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right',
-            }}
-            transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-            }}
-            {...props}
-        />
-    ))(({ theme }) => ({
-        '& .MuiPaper-root': {
-
-            borderRadius: 6,
-            marginTop: theme.spacing(1),
-            minWidth: 180,
-            color:
-                theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
-            boxShadow:
-                'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
-            '& .MuiMenu-list': {
-                padding: '4px 0',
-            },
-            '& .MuiMenuItem-root': {
-                '& .MuiSvgIcon-root': {
-                    fontSize: 18,
-                    color: theme.palette.text.secondary,
-                    marginRight: theme.spacing(1.5),
-                },
-                '&:active': {
-                    backgroundColor: alpha(
-                        theme.palette.primary.main,
-                        theme.palette.action.selectedOpacity,
-                    ),
-                },
-            },
-        },
-    }));
     const [anchorEl, setAnchorEl] = React.useState(null);
     const open = Boolean(anchorEl);
     const handleClick = (event) => {
@@ -110,14 +66,12 @@ const NewMap = () => {
         try {
             if (initCountry) {
                 const res = await axios.get(`/${initCountry}`)
-                console.log({ res })
                 setUserLocation({
                     latitude: res.data[0].latlng[0],
                     longitude: res.data[0].latlng[1],
                 });
                 setInitCountry("")
             }
-            // setcountryData(res.data[0])
         }
         catch (error) {
             console.log({ error })
@@ -135,7 +89,7 @@ const NewMap = () => {
     image.src = 'data:image/svg+xml;charset=utf-8;base64,' + btoa("svg");
     const images = ['londonCycle', image];
 
-    const firstCircle = turf.circle([userLocation.longitude, userLocation.latitude], radius, options);
+    const firstCircle = turf.point([userLocation.longitude, userLocation.latitude], radius * 10, options);
     const renderCountries = () => {
         return (
             <Box mt="20px" textAlign="center">
@@ -192,6 +146,16 @@ const NewMap = () => {
                         zoom={[14]}
                         center={[userLocation.longitude, userLocation.latitude]}
                     >
+                        <Popup
+                            coordinates={[userLocation.longitude, userLocation.latitude]}
+                            offset={{
+                                "bottom-left": [12, -38],
+                                bottom: [0, -38],
+                                "bottom-right": [-12, -38],
+                            }}
+                        >
+                            <h1> This is Awesome</h1>
+                        </Popup>
                         <GeoJSONLayer {...geojsonStyles} data={firstCircle} />
                         <Layer type="symbol" id="marker" layout={layoutLayer}>
                             <Feature
