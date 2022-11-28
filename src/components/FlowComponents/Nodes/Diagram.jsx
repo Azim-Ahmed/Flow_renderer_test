e33de8760b79148cc154107d8a3e992919f8226b
Azim Uddin Ahamed <azimuahmed13@gmail.com>
2022-11-28 14:25:57 +0600

    fixed layout for designing

--- src/components/FlowComponents/Nodes/Diagram.jsx
+++ src/components/FlowComponents/Nodes/Diagram.jsx
@@ -25,109 +25,125 @@ function DiagramNode() {
   console.log(style.width);
   var width = window.innerWidth > 0 ? window.innerWidth : screen.width;
   return (
-    <div className="container">
-      <Layout>
-        <CanvasDraw
-          hideInterface
-          // hideGrid
-          enablePanAndZoom={true}
-          onChange={(e) => console.log(e)}
-          ref={(canvasDraw) => setModify(canvasDraw)}
-          brushColor={canvas}
-          clampLinesToDocument
-          zoomExtents={{ min: 0.33, max: 4 }}
-          brushRadius={brush}
-          lazyRadius={1}
-          // imgSrc="https://upload.wikimedia.org/wikipedia/commons/a/a1/Nepalese_Mhapuja_Mandala.jpg"
-          canvasHeight={850}
-          canvasWidth={width - 100}
-        />
+    <Layout>
+      <CanvasDraw
+        hideInterface
+        // hideGrid
+        enablePanAndZoom={true}
+        onChange={(e) => console.log(e)}
+        ref={(canvasDraw) => setModify(canvasDraw)}
+        brushColor={canvas}
+        clampLinesToDocument
+        zoomExtents={{ min: 0.33, max: 4 }}
+        brushRadius={brush}
+        lazyRadius={1}
+        // imgSrc="https://upload.wikimedia.org/wikipedia/commons/a/a1/Nepalese_Mhapuja_Mandala.jpg"
+        canvasHeight={850}
+        canvasWidth={width - 20}
+      />
 
-        <Button
-          variant="contained"
-          onClick={() => {
-            modify.undo();
+      <Box
+        style={{
+          display: "flex",
+          justifyContent: "center",
+        }}
+      >
+        <Box
+          style={{
+            background: "#F8F8F8",
+            maxWidth: "500px",
+            minWidth: "450px",
+            textAlign: "center",
+            padding: "10px",
+            // display: "flex",
+            // justifyContent: "center",
+            boxShadow: "7px 7px 7px 2px gray",
           }}
         >
-          UNDO
-        </Button>
-        <Button
-          variant="contained"
-          onClick={() =>
-            setShowImage((prev) => ({
-              ...prev,
-              open: true,
-              getData: modify.getDataURL(),
-            }))
-          }
-        >
-          Show
-        </Button>
-        <Button
-          variant="contained"
-          onClick={() => {
-            modify.clear();
-          }}
-        >
-          CLEAR
-        </Button>
-        <br />
-        <label>Colour picker</label>
-        <input
-          style={{ background: { canvas } }}
-          type="color"
-          value={canvas}
-          onChange={(event) => {
-            setBrush(event.target.value);
-          }}
-        />
-
-        <br />
-        <label>Brush Thickness</label>
-        <div
-          className="thickness"
-          style={{ ...style, marginLeft: "40px" }}
-        ></div>
-        <input
-          min="2"
-          max="50"
-          type="range"
-          onChange={(event) => {
-            console.log(event.target.value);
-            setThick(event.target.value);
-          }}
-        />
-
-        {showimage.open && (
-          <TransitionsModal
-            open={showimage.open}
-            handleClose={() =>
-              setShowImage((prev) => ({ ...prev, open: false, getData: "" }))
+          <Button
+            variant="contained"
+            color="warning"
+            onClick={() => {
+              modify.undo();
+            }}
+          >
+            UNDO
+          </Button>
+          <Button
+            variant="contained"
+            color="success"
+            onClick={() =>
+              setShowImage((prev) => ({
+                ...prev,
+                open: true,
+                getData: modify.getDataURL(),
+              }))
             }
           >
-            <img src={showimage.getData} alt="" />
-            <Box
-              display="flex"
-              alignItems={"flex-end"}
-              justifyContent="flex-end"
+            Show
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() => {
+              modify.clear();
+            }}
+          >
+            CLEAR
+          </Button>
+          <br />
+          <label>Colour picker</label>
+          <input
+            style={{ background: { canvas } }}
+            type="color"
+            value={canvas}
+            onChange={(event) => {
+              setBrush(event.target.value);
+            }}
+          />
+
+          <br />
+          <label>Brush Thickness</label>
+          <div
+            className="thickness"
+            style={{ ...style, marginLeft: "40px" }}
+          ></div>
+          <input
+            min="2"
+            max="50"
+            type="range"
+            onChange={(event) => {
+              console.log(event.target.value);
+              setThick(event.target.value);
+            }}
+          />
+        </Box>
+      </Box>
+
+      {showimage.open && (
+        <TransitionsModal
+          open={showimage.open}
+          handleClose={() =>
+            setShowImage((prev) => ({ ...prev, open: false, getData: "" }))
+          }
+        >
+          <img src={showimage.getData} alt="" />
+          <Box display="flex" alignItems={"flex-end"} justifyContent="flex-end">
+            <Button
+              variant="contained"
+              onClick={() =>
+                setShowImage((prev) => ({
+                  ...prev,
+                  open: false,
+                  getData: "",
+                }))
+              }
             >
-              <Button
-                variant="contained"
-                onClick={() =>
-                  setShowImage((prev) => ({
-                    ...prev,
-                    open: false,
-                    getData: "",
-                  }))
-                }
-              >
-                Close
-              </Button>
-            </Box>
-          </TransitionsModal>
-        )}
-      </Layout>
-    </div>
+              Close
+            </Button>
+          </Box>
+        </TransitionsModal>
+      )}
+    </Layout>
   );
 }
 export default DiagramNode;
