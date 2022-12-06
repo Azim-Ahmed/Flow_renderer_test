d644dac19566b93c7b05ed0c00943f9dafcc2c7f
Azim Uddin Ahamed <azimuahmed13@gmail.com>
2022-12-06 22:44:24 +0600

    random color added for custom site generation

--- src/components/FlowComponents/Nodes/Browser.jsx
+++ src/components/FlowComponents/Nodes/Browser.jsx
@@ -7,7 +7,9 @@ export default function ChromeExample({ showHeader = false }) {
   const [tabs, setTabs] = useState([])
   const addTabs = () => {
     const newTab = {
-      id: uuidv4()
+      id: uuidv4(),
+      color: "#" + ((1 << 24) * Math.random() | 0).toString(16).padStart(6, "0"),
+
     }
     setTabs(Prev => [...Prev, newTab])
   }
@@ -34,7 +36,7 @@ export default function ChromeExample({ showHeader = false }) {
           >
             <div
               style={{
-                backgroundColor: 'green',
+                backgroundColor: item.color,
                 height: '100%',
                 width: '100%',
                 opacity: 0.9,
@@ -46,51 +48,7 @@ export default function ChromeExample({ showHeader = false }) {
           </div>
         </Tab>)
         }
-        {/* <Tab key={'green'} imageUrl={''} imageAlt={'green tab image'} title={'Green'}>
-          <div style={{ backgroundColor: 'green', height: '100%', width: '100%', opacity: 0.9, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <h1 style={{ color: 'white', margin: 0 }}>{'Your component here'}</h1>
-          </div>
-        </Tab> */}
-        {/* <Tab key={'blue'} imageAlt={'blue tab image'} title={'Blue'}>
-          <div style={{ backgroundColor: 'green', height: '100%', width: '100%', opacity: 0.9, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <h1 style={{ color: 'white', margin: 0 }}>{'Your component here'}</h1>
-          </div>
-        </Tab> */}
       </Browser>
     </div>
   )
 }
-
-// import React from 'react'
-// import Browser, { Firefox } from 'react-browser-ui'
-
-// const { Tab, Divider, AddButton } = Firefox
-
-// export default function FirefoxExample () {
-//   const tabEnd = (
-//     <React.Fragment>
-//       <Divider />
-//       <AddButton />
-//     </React.Fragment>
-//   )
-//   return (
-//     <div style={{ width: 600, height: 500 }}>
-//       <Browser
-//         type={'firefox'}
-//         showHeader={false}
-//         activeTabKey={'green'}
-//         tabEnd={tabEnd}>
-//         <Tab key={'green'} imageAlt={'green tab image'} title={'Green'}>
-//           <div style={{ backgroundColor: 'green', height: '100%', width: '100%', opacity: 0.9, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-//             <h1 style={{ color: 'white', margin: 0 }}>{'Your component here'}</h1>
-//           </div>
-//         </Tab>
-//         <Tab key={'blue'} imageUrl={''} imageAlt={'blue tab image'} title={'Blue'}>
-//           <div style={{ backgroundColor: 'green', height: '100%', width: '100%', opacity: 0.9, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-//             <h1 style={{ color: 'white', margin: 0 }}>{'Your component here'}</h1>
-//           </div>
-//         </Tab>
-//       </Browser>
-//     </div>
-//   )
-// }
